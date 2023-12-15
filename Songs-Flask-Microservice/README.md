a91c708d88e625492030a61f5ec30f24a0ab7ba6
CASPIANIC\arturk.mammadli <arturk.mammadli@caspianic.com>
2023-12-16 00:05:41 +0400

    Songs microservice in Flask with MongoDB CRUD operations added

---
+++ Songs-Flask-Microservice/README.md
@@ -0,0 +1,7 @@
+# Songs Microservice in Flask
+
+Set up MongoDB database.  
+
+Implement the service to retrieve song lyrics from the database.  
+
+Create health endpoint for the microservice.
---
+++ Songs-Flask-Microservice/backend/routes.py
@@ -0,0 +1,115 @@
+from . import app
+import os
+import json
+import pymongo
+from flask import jsonify, request, make_response, abort, url_for  # noqa; F401
+from pymongo import MongoClient
+from bson import json_util
+from pymongo.errors import OperationFailure
+from pymongo.results import InsertOneResult
+from bson.objectid import ObjectId
+import sys
+
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+json_url = os.path.join(SITE_ROOT, "data", "songs.json")
+songs_list: list = json.load(open(json_url))
+
+# client = MongoClient(
+#     f"mongodb://{app.config['MONGO_USERNAME']}:{app.config['MONGO_PASSWORD']}@localhost")
+mongodb_service = os.environ.get('MONGODB_SERVICE')
+mongodb_username = os.environ.get('MONGODB_USERNAME')
+mongodb_password = os.environ.get('MONGODB_PASSWORD')
+mongodb_port = os.environ.get('MONGODB_PORT')
+
+print(f'The value of MONGODB_SERVICE is: {mongodb_service}')
+
+if mongodb_service == None:
+    app.logger.error('Missing MongoDB server in the MONGODB_SERVICE variable')
+    # abort(500, 'Missing MongoDB server in the MONGODB_SERVICE variable')
+    sys.exit(1)
+
+if mongodb_username and mongodb_password:
+    url = f"mongodb://{mongodb_username}:{mongodb_password}@{mongodb_service}"
+else:
+    url = f"mongodb://{mongodb_service}"
+
+
+print(f"connecting to url: {url}")
+
+try:
+    client = MongoClient(url)
+except OperationFailure as e:
+    app.logger.error(f"Authentication error: {str(e)}")
+
+db = client.songs
+db.songs.drop()
+db.songs.insert_many(songs_list)
+
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+
+######################################################################
+# INSERT CODE HERE
+######################################################################
+
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify(dict(status="OK")), 200
+
+
+@app.route('/count', methods=['GET'])
+def count():
+    count = db.songs.count_documents({})
+    return {"count": count}, 200
+
+
+@app.route('/song')
+def songs():
+    song_files = db.songs.find({})
+    return {'songs': parse_json(song_files)}, 200
+
+
+@app.route('/song/<int:id>', methods=['GET'])
+def get_song_by_id(id):
+    try:
+        song = db.songs.find_one({'id': id})
+        return {'song': parse_json(song)}, 200
+    except:
+        return {"message": f"song with id {id} not found"}, 404 
+
+
+@app.route('/song', methods=['POST'])
+def create_song():
+    new_song = request.json
+    songs = db.songs.find({})
+
+    for song in songs:
+        if song['id'] == new_song.get('id'):
+            return {"Message": f"song with id {song['id']} already present"}, 302
+        
+    db.songs.insert_one(new_song)
+    return {"Message": f"song with id {song['id']} successfully added"}, 200
+
+
+@app.route('/song/<int:id>', methods=['PUT'])
+def update_song(id):
+    data = request.json
+
+    try:
+        song = db.songs.find_one({'id': id})
+        updated_song = db.songs.update_one({'id': id}, {'$set': data})
+        if updated_song.matched_count > 0 and updated_song.modified_count > 0:
+            return {"message": "song successfully updated"}, 200
+        else:
+            return {"message":"song found, but nothing updated"}, 200
+    except:
+        return {"message": "song not found"}, 404
+
+
+@app.route('/song/<int:id>', methods=['DELETE'])
+def delete_song(id):
+    deleted = db.songs.delete_one({'id': id})
+    if deleted.deleted_count:
+        return {"message": "song successfully deleted"}, 200
+    else:
+        return {"message": "song not found"}, 404
